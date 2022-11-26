Fix normalization and batch

transformer/train.py
@@ -21,78 +21,9 @@ transformers = transforms.Compose(
         transforms.Resize((imgWidth, imgHeight)),
         transforms.ToTensor(),
         # normalize
-        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+        transforms.Normalize([0.5], [0.5])
     ]
 )
-
-# def generate_random_data(n):
-#     SOS_token = np.array([2])
-#     EOS_token = np.array([3])
-#     length = 8
-
-#     data = []
-
-#     # 1,1,1,1,1,1 -> 1,1,1,1,1
-#     for i in range(n // 3):
-#         X = np.concatenate((SOS_token, np.ones(length), EOS_token))
-#         y = np.concatenate((SOS_token, np.ones(length), EOS_token))
-#         data.append([X, y])
-
-#     # 0,0,0,0 -> 0,0,0,0
-#     for i in range(n // 3):
-#         X = np.concatenate((SOS_token, np.zeros(length), EOS_token))
-#         y = np.concatenate((SOS_token, np.zeros(length), EOS_token))
-#         data.append([X, y])
-
-#     # 1,0,1,0 -> 1,0,1,0,1
-#     for i in range(n // 3):
-#         X = np.zeros(length)
-#         start = random.randint(0, 1)
-
-#         X[start::2] = 1
-
-#         y = np.zeros(length)
-#         if X[-1] == 0:
-#             y[::2] = 1
-#         else:
-#             y[1::2] = 1
-
-#         X = np.concatenate((SOS_token, X, EOS_token))
-#         y = np.concatenate((SOS_token, y, EOS_token))
-
-#         data.append([X, y])
-
-#     np.random.shuffle(data)
-
-#     return data
-
-
-# def batchify_data(data, batch_size=16, padding=False, padding_token=-1):
-#     batches = []
-#     for idx in range(0, len(data), batch_size):
-#         # We make sure we dont get the last bit if its not batch_size size
-#         if idx + batch_size < len(data):
-#             # Here you would need to get the max length of the batch,
-#             # and normalize the length with the PAD token.
-#             if padding:
-#                 max_batch_length = 0
-
-#                 # Get longest sentence in batch
-#                 for seq in data[idx : idx + batch_size]:
-#                     if len(seq) > max_batch_length:
-#                         max_batch_length = len(seq)
-
-#                 # Append X padding tokens until it reaches the max length
-#                 for seq_idx in range(batch_size):
-#                     remaining_length = max_batch_length - len(data[idx + seq_idx])
-#                     data[idx + seq_idx] += [padding_token] * remaining_length
-
-#             batches.append(np.array(data[idx : idx + batch_size]).astype(np.int64))
-
-#     print(f"{len(batches)} batches of size {batch_size}")
-
-#     return batches
-
 
 
 
@@ -101,8 +32,11 @@ def train_loop(model, opt, loss_fn, dataloader, device):
     total_loss = 0
 
     for batch in dataloader:
-        x = torch.tensor(batch[:, 0]).to(device)
-        y = torch.tensor(batch[:, 1]).to(device)
+        # x = torch.tensor(batch[:, 0], dtype=torch.long, device=device)
+        # y = torch.tensor(batch[:, 1], dtype=torch.long, device=device)
+
+        x = torch.tensor(batch["Image"], dtype=torch.long, device=device)
+        y = torch.tensor(batch["Truth"], dtype=torch.long, device=device)
 
         # Shift tgt input by 1 for prediction
         y_input = y[:, :-1]
@@ -134,8 +68,11 @@ def validation_loop(model, loss_fn, dataloader, device):
 
     with torch.no_grad():
         for batch in dataloader:
-            x = torch.tensor(batch[:, 0], dtype=torch.long, device=device)
-            y = torch.tensor(batch[:, 1], dtype=torch.long, device=device)
+            # x = torch.tensor(batch[:, 0], dtype=torch.long, device=device)
+            # y = torch.tensor(batch[:, 1], dtype=torch.long, device=device)
+
+            x = torch.tensor(batch["Image"], dtype=torch.long, device=device)
+            y = torch.tensor(batch["Truth"], dtype=torch.long, device=device)
 
             # Shift tgt input by 1 for prediction
             y_input = y[:, :-1]
@@ -255,3 +192,75 @@ def train(model, device):
     #     print(f"Input: {example.view(-1).tolist()[1:-1]}")
     #     print(f"Continuation: {result[1:-1]}")
     #     print()
+
+
+
+    
+# def generate_random_data(n):
+#     SOS_token = np.array([2])
+#     EOS_token = np.array([3])
+#     length = 8
+
+#     data = []
+
+#     # 1,1,1,1,1,1 -> 1,1,1,1,1
+#     for i in range(n // 3):
+#         X = np.concatenate((SOS_token, np.ones(length), EOS_token))
+#         y = np.concatenate((SOS_token, np.ones(length), EOS_token))
+#         data.append([X, y])
+
+#     # 0,0,0,0 -> 0,0,0,0
+#     for i in range(n // 3):
+#         X = np.concatenate((SOS_token, np.zeros(length), EOS_token))
+#         y = np.concatenate((SOS_token, np.zeros(length), EOS_token))
+#         data.append([X, y])
+
+#     # 1,0,1,0 -> 1,0,1,0,1
+#     for i in range(n // 3):
+#         X = np.zeros(length)
+#         start = random.randint(0, 1)
+
+#         X[start::2] = 1
+
+#         y = np.zeros(length)
+#         if X[-1] == 0:
+#             y[::2] = 1
+#         else:
+#             y[1::2] = 1
+
+#         X = np.concatenate((SOS_token, X, EOS_token))
+#         y = np.concatenate((SOS_token, y, EOS_token))
+
+#         data.append([X, y])
+
+#     np.random.shuffle(data)
+
+#     return data
+
+
+# def batchify_data(data, batch_size=16, padding=False, padding_token=-1):
+#     batches = []
+#     for idx in range(0, len(data), batch_size):
+#         # We make sure we dont get the last bit if its not batch_size size
+#         if idx + batch_size < len(data):
+#             # Here you would need to get the max length of the batch,
+#             # and normalize the length with the PAD token.
+#             if padding:
+#                 max_batch_length = 0
+
+#                 # Get longest sentence in batch
+#                 for seq in data[idx : idx + batch_size]:
+#                     if len(seq) > max_batch_length:
+#                         max_batch_length = len(seq)
+
+#                 # Append X padding tokens until it reaches the max length
+#                 for seq_idx in range(batch_size):
+#                     remaining_length = max_batch_length - len(data[idx + seq_idx])
+#                     data[idx + seq_idx] += [padding_token] * remaining_length
+
+#             batches.append(np.array(data[idx : idx + batch_size]).astype(np.int64))
+
+#     print(f"{len(batches)} batches of size {batch_size}")
+
+#     return batches
+