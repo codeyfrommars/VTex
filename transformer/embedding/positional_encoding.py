4810e75f9d4da6de7b44bf0de1bcdccfe01e1e2e
codeyfrommars <codey.sun@gmail.com>
2022-12-03 03:42:26 -0600

    Fixed some tensors not on same device

--- transformer/embedding/positional_encoding.py
+++ transformer/embedding/positional_encoding.py
@@ -6,7 +6,7 @@ class PositionalEncoding(nn.Module):
     """
     sinusoidal positional encoding, as described in Attention is All You Need
     """
-    def __init__(self, dim_model, max_length):
+    def __init__(self, dim_model, max_length, device):
         """
         max_length is the max sequence length of your data (gets rid of outliers)
 
@@ -14,12 +14,14 @@ class PositionalEncoding(nn.Module):
         positional encodings in both the encoder and decoder stacks.
         """
         super(PositionalEncoding, self).__init__()
-        pe = torch.zeros(max_length, dim_model)
+        
+        self.device = device
+        pe = torch.zeros(max_length, dim_model, device=self.device)
 
-        position = torch.arange(start=0, end=max_length, dtype=torch.float)
+        position = torch.arange(start=0, end=max_length, dtype=torch.float, device=self.device)
         # change position from shape [max_length] into shape [max_length, 1] to allow for matrix multiplication
         position = position.unsqueeze(1)
-        dimension = torch.arange(start=0, end=dim_model, step=2, dtype=torch.float)
+        dimension = torch.arange(start=0, end=dim_model, step=2, dtype=torch.float, device=self.device)
         div_term = 1.0 / (10000.0 ** (dimension / dim_model))
         # change div_term from shape [dim_model] to [1, dim_model]
         div_term = div_term.unsqueeze(0)
